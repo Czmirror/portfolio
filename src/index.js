eca9001da00cdf7d5a9539ca06ae85325f518762
Kazushige Mori <mori@sonobuoy.co.jp>
2024-07-21 01:55:35 +0900

    Whats new修正

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import Updates from './components/Updates';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Updates />
-    </React.StrictMode>,
+    React.createElement(
+        React.StrictMode,
+        null,
+        React.createElement(Updates, null)
+    ),
     document.getElementById('root')
 );
