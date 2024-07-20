384c9c2f2578a99a47bf37ed1453d635bca46282
Kazushige Mori <mori@sonobuoy.co.jp>
2024-07-21 02:38:03 +0900

    github page関連設定

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
+    base: process.env.GITHUB_PAGES ? 'REPOSITORY_NAME' : './',
+    root: 'src',
+    plugins: [react({ jsxImportSource: '@emotion/react' })]
     resolve: {
         alias: {
             '@': '/src',
